d3bf715dd5707009bca5e9acbdf8a992dc29a357
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2024-10-17 15:57:16 -0300

    5

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+// app.js
+
 const express = require('express');
-
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000; // O el puerto que estés usando
 
-// Configurar la ruta principal
+// Servir archivos estáticos desde el directorio "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal ("/") que sirve el archivo index.html
 app.get('/', (req, res) => {
-    try {
-        res.send('Hola Mundo!');
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
-    }
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Ruta para "/inicio" que sirve el archivo inicio.html
+app.get('/inicio', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'inicio.html'));
+});
 
 // Iniciar el servidor
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
