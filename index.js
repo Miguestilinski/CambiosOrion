0af6c58c4129b294c91dc3d452528f0bfacb5b4a
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2024-10-17 14:06:00 -0300

    4.3

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const express = require('express');
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Configurar la ruta principal
+app.get('/', (req, res) => {
+    try {
+        res.send('Hola Mundo!');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
