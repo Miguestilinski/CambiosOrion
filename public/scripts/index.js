e2d944a172b71c5b29d35e2a6b92ea445a036bda
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2024-10-22 13:05:56 -0300

    8.5

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -21,7 +21,7 @@ function loadCurrencies() {
             data.forEach(divisa => {
                 // Crear opción para currency1
                 const option1 = document.createElement("div");
-                option1.innerHTML = `<img src="${divisa.icono}" alt="${divisa.nombre}" class="w-5 h-5 mr-2"> ${divisa.nombre}`;
+                option1.innerHTML = `<img src="/icons/${divisa.icono}" alt="${divisa.nombre}" class="w-5 h-5 mr-2"> ${divisa.nombre}`; // Asegúrate de que la ruta sea correcta
                 option1.className = "p-2 hover:bg-gray-100 cursor-pointer";
                 option1.onclick = function () {
                     document.getElementById("currency1").textContent = divisa.nombre;
@@ -38,7 +38,7 @@ function loadCurrencies() {
 
                 // Crear opción para currency2
                 const option2 = document.createElement("div");
-                option2.innerHTML = `<img src="${divisa.icono}" alt="${divisa.nombre}" class="w-5 h-5 mr-2"> ${divisa.nombre}`;
+                option2.innerHTML = `<img src="/icons/${divisa.icono}" alt="${divisa.nombre}" class="w-5 h-5 mr-2"> ${divisa.nombre}`; // Asegúrate de que la ruta sea correcta
                 option2.className = "p-2 hover:bg-gray-100 cursor-pointer";
                 option2.onclick = function () {
                     document.getElementById("currency2").textContent = divisa.nombre;
