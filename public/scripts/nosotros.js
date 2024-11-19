5ebc478e596b344725f1dec1a94e9cb28f32f12a
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2024-11-19 10:57:42 -0300

    12.4

---
+++ public/scripts/nosotros.js
@@ -0,0 +1,49 @@
+document.addEventListener('click', function (event) {
+    const navMenuButton = document.getElementById('nav-menu-button');
+    const sessionMenuButton = document.getElementById('session-menu-button');
+    const navMobileMenu = document.getElementById('nav-mobile-menu');
+    const sessionMobileMenu = document.getElementById('session-mobile-menu');
+
+    if (navMenuButton && sessionMenuButton && navMobileMenu && sessionMobileMenu) {
+        navMenuButton.addEventListener('click', () => {
+            toggleMenu(navMobileMenu);
+            if (sessionMobileMenu && sessionMobileMenu.style && sessionMobileMenu.style.display === 'block') {
+                sessionMobileMenu.style.display = 'none';
+            }            
+        });
+
+        sessionMenuButton.addEventListener('click', () => {
+            toggleMenu(sessionMobileMenu);
+            if (navMobileMenu && navMobileMenu.style.display === 'block') {
+                navMobileMenu.style.display = 'none';
+            }
+        });
+    }
+
+    setActiveLink('#nav-menu');
+    setActiveLink('#session-menu');
+});
+
+// Función para alternar visibilidad
+function toggleMenu(menu) {
+    if (menu) {
+        menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+    }
+}
+
+// Marcar la opción activa en el menú
+function setActiveLink(menuId) {
+    const links = document.querySelectorAll(`${menuId} a`);
+    const currentPath = window.location.pathname;
+    links.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('selected');
+        } else {
+            link.classList.remove('selected');
+        }
+    });
+}
+
+window.addEventListener('resize', function () {
+    const mobileButtons = document.querySelector('.md\\:hidden');
+});
