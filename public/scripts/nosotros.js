21789180bde21247515ee0302583fe63f349f388
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2024-11-21 15:19:52 -0300

    12.5

--- public/scripts/nosotros.js
+++ public/scripts/nosotros.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", async function () {
+    const navMenuButton = document.getElementById('nav-menu-button');
+    const sessionMenuButton = document.getElementById('session-menu-button');
+    const navMobileMenu = document.getElementById('nav-mobile-menu');
+    const sessionMobileMenu = document.getElementById('session-mobile-menu');
+
     const placeId = "ChIJVc2tm-bFYpYRaDgA0qs7CvM";
     const apiKey = "AIzaSyDNWdnOEsPOqlKvBHcg2AN7YY5AGlZ5fcM";
     const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`;
@@ -80,5 +85,47 @@ document.addEventListener("DOMContentLoaded", async function () {
         return days;
     }
 
+    if (navMenuButton && sessionMenuButton && navMobileMenu && sessionMobileMenu) {
+        navMenuButton.addEventListener('click', () => {
+            toggleMenu(navMobileMenu);
+            if (sessionMobileMenu && sessionMobileMenu.style && sessionMobileMenu.style.display === 'block') {
+                sessionMobileMenu.style.display = 'none';
+            }            
+        });
+
+        sessionMenuButton.addEventListener('click', () => {
+            toggleMenu(sessionMobileMenu);
+            if (navMobileMenu && navMobileMenu.style.display === 'block') {
+                navMobileMenu.style.display = 'none';
+            }
+        });
+    }
+
     fetchPlaceDetails();
+    setActiveLink('#nav-menu');
+    setActiveLink('#session-menu');
+});
+
+// Función para alternar visibilidad
+function toggleMenu(menu) {
+    if (menu) {
+        menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+    }
+}
+
+// Marcar la opción activa en el menú
+function setActiveLink(menuId) {
+    const links = document.querySelectorAll(`${menuId} a`);
+    const currentPath = window.location.pathname;
+    links.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('selected');
+        } else {
+            link.classList.remove('selected');
+        }
+    });
+}
+
+window.addEventListener('resize', function () {
+    const mobileButtons = document.querySelector('.md\\:hidden');
 });
