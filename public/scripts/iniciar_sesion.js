9ebe5ae51372123c5250e12208244097f1cbddea
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2024-12-12 10:13:18 -0300

    16.1

--- public/scripts/iniciar_sesion.js
+++ public/scripts/iniciar_sesion.js
@@ -66,6 +66,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
         formData.append("correo", correo);
 
+        console.log("Correo recibido: " . $correo);
+        console.log("Campo correo en DB: " . $administrativo['correo']);
+
         try {
             const response = await fetch('https://cambiosorion.cl/data/iniciar_sesion.php', {
                 method: 'POST',
