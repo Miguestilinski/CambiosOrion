ebeef04d65c079c3a14773f8077e353ff991e13d
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2025-01-02 17:06:42 -0300

    17.9

--- public/scripts/form_doc.js
+++ public/scripts/form_doc.js
@@ -1,4 +1,4 @@
-import { Country, State, City } from "country-state-city";
+import { Country, State, City } from "/orionapp/mode-modules/country-state-city";
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("dynamic-form");
