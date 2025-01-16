587a348e9a84635f9ea17c6139f372caee713d5d
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2025-01-16 15:43:14 -0300

    19.4

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -472,11 +472,17 @@ function fillCurrencyTable() {
                     `}
                 </td>
                 <td class="px-4 py-2 edit-column ${isEditMode ? '' : 'hidden'}">
+                ${
+                    currency === 'CLP'
+                        ? '' // No mostrar botón de eliminación para CLP
+                        : `
                     <button onclick="deleteCurrency('${currency}')" class="delete-btn">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-9 h-9 text-white">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                         </svg>
                     </button>
+                            `
+                    }
                 </td>
             `;
 
