4bf9985c10da73298e1236d26d4aa30aff938d0e
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2025-01-23 17:14:46 -0300

    20.2

--- subdominios/admin/scripts/login.js
+++ subdominios/admin/scripts/login.js
@@ -1,64 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const clienteTab = document.getElementById('clienteTab');
     const administrativoTab = document.getElementById('administrativoTab');
-    const clienteForm = document.getElementById('cliente-form');
     const administrativoForm = document.getElementById('administrativo-form');
     
     // Establecer "Cliente" como la pestaña predeterminada
-    clienteTab.classList.add('active');
-    clienteForm.classList.add('active');
+    administrativoTab.classList.add('active');
+    administrativoForm.classList.add('active');
     
-    // Cambiar entre Cliente y Administrativo
-    clienteTab.addEventListener('click', function() {
-        cambiarVista('cliente');
-    });
-
-    administrativoTab.addEventListener('click', function() {
-        cambiarVista('administrativo');
-    });
-
-    function cambiarVista(tipo) {
-        if (tipo === 'cliente') {
-            clienteTab.classList.add('active');
-            administrativoTab.classList.remove('active');
-            clienteForm.classList.add('active');
-            administrativoForm.classList.remove('active');
-            document.getElementById('rut').setAttribute('required', 'required');
-            document.getElementById('correo').removeAttribute('required');
-            document.getElementById('tipoUsuario').value = 'cliente';
-        } else {
-            administrativoTab.classList.add('active');
-            clienteTab.classList.remove('active');
-            administrativoForm.classList.add('active');
-            clienteForm.classList.remove('active');
-            document.getElementById('correo').setAttribute('required', 'required');
-            document.getElementById('rut').removeAttribute('required');
-            document.getElementById('tipoUsuario').value = 'administrativo';
-        }
-    }
-
-    const rutInput = document.getElementById("rut");
-
-    rutInput.addEventListener("blur", function() {
-        // Eliminar puntos, guiones y espacios del RUT ingresado
-        let rut = rutInput.value.replace(/[^\dKk]/g, "").toUpperCase();
-
-        // Si el RUT tiene menos de 2 caracteres, no es válido para formatear
-        if (rut.length < 2) {
-            rutInput.value = rut; // Mostrarlo tal cual si no es formateable
-            return;
-        }
-
-        const cuerpo = rut.slice(0, -1);
-        const dv = rut.slice(-1);
-
-        // Formatear el cuerpo del RUT con puntos cada tres dígitos
-        const cuerpoFormateado = cuerpo.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
-        // Actualizar el campo con el formato completo
-        rutInput.value = `${cuerpoFormateado}-${dv}`;
-    });
-
     const loginForm = document.getElementById("loginForm");
     loginForm.addEventListener("submit", async function(event) {
         event.preventDefault();
@@ -71,11 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // Convertir el correo a minúsculas automáticamente
         correo = correo.toLowerCase();
 
-        // Validación de RUT si es un cliente
-        if (tipoUsuario === 'cliente' && !validarRUT(rut)) {
-            alert("El RUT no es válido.");
-            return;
-        } else if (tipoUsuario === 'administrativo' && correo === "") {
+        if (tipoUsuario === 'administrativo' && correo === "") {
             alert("Por favor, ingresa un correo válido.");
             return;
         }
@@ -100,10 +43,10 @@ document.addEventListener("DOMContentLoaded", function() {
             
             if (result.success) {
                 localStorage.setItem('sessionActive', 'true');
-                if (tipoUsuario === 'cliente') {
+                if (tipoUsuario === 'administrativo') {
                     window.location.href = "index";
-                } else if (tipoUsuario === 'administrativo') {
-                    window.location.href = "edit";
+                } else if (tipoUsuario === 'cliente') {
+                    window.location.href = "https://cambiosorion.cl/sin-acceso";
                 }
             } else {
                 alert(result.message);
@@ -142,27 +85,18 @@ document.addEventListener("DOMContentLoaded", function() {
 // Función para restablecer los estilos de error
 function resetErrorStyles() {
     // Elimina las clases de error
-    const rutError = document.getElementById('rut-error');
     const passwordError = document.getElementById('password-error');
-    if (rutError) rutError.classList.add('hidden');
     if (passwordError) passwordError.classList.add('hidden');
 
-    const rutInput = document.getElementById('rut');
     const correoInput = document.getElementById('correo');
     const passwordInput = document.getElementById('password');
-    if (rutInput) rutInput.classList.remove('bg-red-50', 'border-red-500', 'text-red-900');
     if (correoInput) correoInput.classList.remove('bg-red-50', 'border-red-500', 'text-red-900');
     if (passwordInput) passwordInput.classList.remove('bg-red-50', 'border-red-500', 'text-red-900');
 }
 
 // Función para aplicar estilos de error
 function setErrorStyles(field) {
-    if (field === 'rut') {
-        const rutInput = document.getElementById('rut');
-        const rutError = document.getElementById('rut-error');
-        if (rutInput) rutInput.classList.add('bg-red-50', 'border-red-500', 'text-red-900');
-        if (rutError) rutError.classList.remove('hidden');
-    } else if (field === 'correo') {
+    if (field === 'correo') {
         const correoInput = document.getElementById('correo');
         const correoError = document.getElementById('correo-error');
         if (correoInput) correoInput.classList.add('bg-red-50', 'border-red-500', 'text-red-900');
