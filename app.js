fd623a8055f19aa268fdb1c8bb3d8dc1a7915205
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2025-01-23 11:26:15 -0300

    20.1

--- app.js
+++ app.js
@@ -8,26 +8,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3306;
 
-const allowedOrigins = [
-  'https://pizarras.cambiosorion.cl',
-  'https://admin.cambiosorion.cl',
-  'https://cambiosorion.cl',
-];
-
 // Cargar las variables de entorno
 require('dotenv').config();
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin); // Devuelve el origen permitido
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
