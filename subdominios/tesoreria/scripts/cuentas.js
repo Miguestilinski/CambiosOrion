7153bf91c685a39472ff1f8fd48e46b746ed96f8
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2025-04-02 14:04:57 -0300

    22.3

--- subdominios/tesoreria/scripts/cuentas.js
+++ subdominios/tesoreria/scripts/cuentas.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         Mostrar
                     </button>
                 </td>
-                <td class="px-4 py-2">
+                <td class="px-4 py-2"> 
                     <button class="text-white bg-black hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-1">
                         Editar
                     </button>
