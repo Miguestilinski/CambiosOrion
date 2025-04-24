d1a29be5b7b293306d376f71acea7b85246ac342
Miguestilinski <m.muozmuoz11@uandresbello.edu>
2025-04-24 18:51:09 -0400

    30.0

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -106,11 +106,15 @@ function removeTrailingZeros(value) {
     return floatValue.toString();
 }
 
+let eventSource;
+let retryTimeout;
+
 function initializeSSE() {
-    const eventSource = new EventSource('https://cambiosorion.cl/api/divisas/stream/stream_divisas.php');
+    eventSource = new EventSource('https://cambiosorion.cl/data/stream_divisas.php');
 
     eventSource.onopen = () => {
         console.log('Conexión SSE establecida correctamente.');
+        clearTimeout(retryTimeout);
     };
 
     eventSource.onmessage = (event) => {
@@ -155,6 +159,9 @@ function initializeSSE() {
     eventSource.onerror = (error) => {
         console.error('Error con la conexión SSE:', error);
         eventSource.close(); // Cerrar la conexión si algo sale mal
+        retryTimeout = setTimeout(() => {
+            initializeSSE();
+        }, 5000);
     };
 }
 
