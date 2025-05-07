9732c4688d3ad48ad97afd83d1e815a182d808ec
Miguestilinski <migue.munozm@hotmail.com>
2025-05-07 18:20:18 -0400

    30.9

--- subdominios/tesoreria/scripts/cuentas.js
+++ subdominios/tesoreria/scripts/cuentas.js
@@ -60,11 +60,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         Mostrar
                     </button>
                 </td>
-                <td class="px-4 py-2"> 
-                    <button class="text-white bg-black hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-1">
-                        Editar
-                    </button>
-                </td>
                 <td class="px-4 py-2">
                     <button class="text-white bg-red-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-1">
                         Desactivar
