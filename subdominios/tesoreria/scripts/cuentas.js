00776d5078cfe6a251f20b11bff522ce12ea2cf6
Miguestilinski <migue.munozm@hotmail.com>
2025-06-08 18:01:34 -0400

    32.9

--- subdominios/tesoreria/scripts/cuentas.js
+++ subdominios/tesoreria/scripts/cuentas.js
@@ -58,9 +58,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 <td class="px-4 py-2">${limpiarTexto(cuenta.divisa)}</td>
                 <td class="px-4 py-2 text-right">${cuenta.me_deben}</td>
                 <td class="px-4 py-2 text-right">${cuenta.debo}</td>
-                <td class="px-4 py-2 text-right">${limpiarTexto(cuenta.por_cobrar_texto)}</td>
+                <td class="px-4 py-2 text-center">${limpiarTexto(cuenta.por_cobrar_texto)}</td>
                 <td class="px-4 py-2 text-center">${limpiarTexto(cuenta.por_pagar_texto)}</td>
-                <td class="px-4 py-2 text-center">${limpiarTexto(cuenta.activa_texto)}</td>
+                <td class="px-4 py-2">${limpiarTexto(cuenta.activa_texto)}</td>
                 <td class="px-4 py-2">
                     <button class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-1">
                         Mostrar
