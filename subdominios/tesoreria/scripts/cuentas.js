606219852f0470e453a9a1d1bfdb6548a1af3e38
Miguestilinski <migue.munozm@hotmail.com>
2025-06-07 21:27:45 -0400

    32.7

--- subdominios/tesoreria/scripts/cuentas.js
+++ subdominios/tesoreria/scripts/cuentas.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const tr = document.createElement('tr');
             tr.classList.add('border-b', 'bg-white', 'border-gray-700', 'text-gray-700');
             tr.innerHTML = `
-                <td class="px-4 py-2">${limpiarTexto(cuenta.id)}</td>
+                <td class="px-4 py-2">${cuenta.id}</td>
                 <td class="px-4 py-2">${limpiarTexto(cuenta.nombre)}</td>
                 <td class="px-4 py-2">${limpiarTexto(cuenta.divisa)}</td>
                 <td class="px-4 py-2">${cuenta.me_deben}</td>
