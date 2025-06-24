2f728a06494e9c92fcd622507986c05687f7b6c6
Miguestilinski <migue.munozm@hotmail.com>
2025-06-24 15:37:45 -0400

    34.6

--- subdominios/tesoreria/scripts/posiciones.js
+++ subdominios/tesoreria/scripts/posiciones.js
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 const tr = document.createElement("tr");
                 tr.className = "border-b bg-white border-gray-700 text-gray-700";
                 tr.innerHTML = `
+                    <td class="px-4 py-2">
+                        <img src="${item.icono}" alt="${item.divisa}" class="w-6 h-6 rounded-full border border-gray-400" />
+                    </td>
                     <td class="px-4 py-2">${item.divisa}</td>
                     <td class="px-4 py-2">${Number(item.cantidad).toLocaleString("es-CL")}</td>
                     <td class="px-4 py-2">$ ${Number(item.pmp).toLocaleString("es-CL", { minimumFractionDigits: 4 })}</td>
