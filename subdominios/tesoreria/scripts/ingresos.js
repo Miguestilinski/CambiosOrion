dc011291e42ea61eaf19fe3847accf1c81004870
Miguestilinski <migue.munozm@hotmail.com>
2025-11-17 21:15:02 -0300

    37.1

--- subdominios/tesoreria/scripts/ingresos.js
+++ subdominios/tesoreria/scripts/ingresos.js
@@ -65,6 +65,16 @@ document.addEventListener('DOMContentLoaded', () => {
         lista.forEach(item => {
             const tr = document.createElement('tr');
             tr.classList.add('border-b', 'bg-white', 'border-gray-700', 'text-gray-700');
+
+            const btnMostrar = document.createElement('button');
+            btnMostrar.textContent = 'Mostrar';
+            btnMostrar.className = 'text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-1';
+            
+            // Añadir evento de redirección
+            btnMostrar.addEventListener('click', () => {
+                window.location.href = `detalle-ing?id=${item.id}`;
+            });
+            
             tr.innerHTML = `
                 <td class="px-4 py-2">${item.id}</td>
                 <td class="px-4 py-2">${formatDate(item.fecha)}</td>
